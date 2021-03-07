522d758620913bb575051ccd81c4be0b8358f68d
NataliyaKurdenkova <natasha_s.86@mail.ru>
2021-03-07 23:08:09 +0300

    homework1

--- src/Lesson1/Fruit/Main.java
+++ src/Lesson1/Fruit/Main.java
@@ -16,36 +16,36 @@ public class Main {
     public static void main(String[] args) {
 
         //с апельсинами
-        Orange fr=new Orange();
-        Orange fr5=new Orange();
-        Orange fr9=new Orange();
-        Box<Orange> box1=new Box<>(fr,fr5,fr9);
-        Orange fr2=new Orange();
+        Orange fr = new Orange();
+        Orange fr5 = new Orange();
+        Orange fr9 = new Orange();
+        Box<Orange> box1 = new Box<>(fr, fr5, fr9);
+        Orange fr2 = new Orange();
         box1.addFruit(fr2);
         System.out.println(box1.getWeight());
         box1.printBox();
 
         //с яблоками
-        Apple fr3=new Apple();
-        Box<Apple> box2=new Box<>(fr3);
-        Apple fr4=new Apple();
+        Apple fr3 = new Apple();
+        Box<Apple> box2 = new Box<>(fr3);
+        Apple fr4 = new Apple();
         box2.addFruit(fr4);
         System.out.println(box2.getWeight());
         box2.printBox();
-       // Orange fr8=new Orange();
-       // box2.addFruit(fr8);
-       // System.out.println(box2.getWeight());
-       // box1.printBox();
+        // Orange fr8=new Orange();
+        // box2.addFruit(fr8);
+        // System.out.println(box2.getWeight());
+        // box1.printBox();
 
         //пустая коробка
-        Box<Fruit> box=new Box<>();
+        Box<Fruit> box = new Box<>();
         System.out.println(box.getWeight());
         box.printBox();
-        Apple fr6=new Apple();
+        Apple fr6 = new Apple();
         box.addFruit(fr6);
         System.out.println(box.getWeight());
         box.printBox();
-        Orange fr7=new Orange();
+        Orange fr7 = new Orange();
         box.addFruit(fr7);
         System.out.println(box.getWeight());
         box.printBox();
@@ -53,9 +53,9 @@ public class Main {
         System.out.println(box.compare(box2));
 
 
-        Apple fr10=new Apple();
-        Box<Apple> box4=new Box<>(fr10);
-        Apple fr11=new Apple();
+        Apple fr10 = new Apple();
+        Box<Apple> box4 = new Box<>(fr10);
+        Apple fr11 = new Apple();
         box4.addFruit(fr11);
         System.out.println(box4.getWeight());
         box4.printBox();
@@ -64,6 +64,5 @@ public class Main {
         box4.put(box2);
         box4.printBox();
         box2.printBox();
-
     }
 }
